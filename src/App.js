ed8e39d012313a68968ef01429bafc2d31242ee4
Gavin <gavin.lui@gmail.com>
2020-11-01 18:48:38 +1100

    minor change to text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Footer from './components/Footer';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
+        <div>test </div>
         <Footer></Footer>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { useState } from "react";
-import { useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import { Typography } from "@material-ui/core";
 
 const Footer = () => {
 
     return (
         <div className="footer">
-            <Typography>Made with love in Melbourne. </Typography>
-            <Typography>&#169; 2020 Broccoli &amp; Co. All rights reserved. </Typography>
+            <Typography variant="body1">Made with 💚 in Melbourne. </Typography>
+            <Typography variant="body1">&#169; 2020 Broccoli &amp; Co. All rights reserved. </Typography>
         </div>
     )
 }
