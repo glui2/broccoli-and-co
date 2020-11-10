c15035846e5cc8810a8b420dbfad821a865d6f28
Gavin <gavin.lui@gmail.com>
2020-11-11 02:12:54 +1100

    add logo to header bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { useState } from "react";
-import { useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { Typography } from "@material-ui/core";
+import { Icon, Typography } from "@material-ui/core";
+import Logo from "../../images/broccoli_logo.svg";
 import "./Header.scss";
 
 const Header = () => {
+  return (
+    <div className="header">
+      <AppBar className="header" color="secondary">
+        <Toolbar>
+          <Typography variant="h6" color="primary" className="company-name">
+            Broccoli &amp; Co.
+          </Typography>
+          <div className="icon-logo">
+            <img src={Logo}></img>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
-    return (
-        <div className="header">
-            <AppBar 
-                className="header" 
-                color="secondary"
-            >
-                <Toolbar>
-                    <Typography variant="h6" color="primary">Broccoli &amp; Co.</Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
-
-export default Header; 
+export default Header;
